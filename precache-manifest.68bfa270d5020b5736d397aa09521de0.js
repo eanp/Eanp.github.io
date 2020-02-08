ca41e7a5d0a960af5ca297aca6d173da1a3391d6
eanp <57554775+eanp@users.noreply.github.com>
2020-02-08 21:42:40 +0700

    Add files via upload

---
+++ precache-manifest.68bfa270d5020b5736d397aa09521de0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b358cf17ee9792372ec5286bca6fa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67027f198f4c2c9f3d31",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5f97494fc9bd93640883",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "67027f198f4c2c9f3d31",
+    "url": "/static/js/2.c6f7d467.chunk.js"
+  },
+  {
+    "revision": "df5f202e1ac2997c0410de98c73fcbc8",
+    "url": "/static/js/2.c6f7d467.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f97494fc9bd93640883",
+    "url": "/static/js/main.946da86d.chunk.js"
+  },
+  {
+    "revision": "070c0282822dc445a026",
+    "url": "/static/js/runtime-main.a4774d23.js"
+  }
+]);
